b0c17a561658600bfbca663a6180d49ebc8a89f0
LexJoss <ajoss@hotmail.co.uk>
2023-02-07 16:32:55 +0000

    added conditional logic to stop singleReview failure to load. Comments added to reviews that contain them

--- src/components/comments.jsx
+++ src/components/comments.jsx
@@ -1,51 +1,48 @@
-import {getReviewsByID} from '../utils/api'
+import {getCommentsByRID} from '../utils/api'
 import {useState, useEffect} from 'react'
 import { Route, Routes, Link, useParams } from 'react-router-dom';
 
 
-export const Comments = (checkData) => {
+export const Comments = (props) => {
 
-    console.log(checkData)
+    
+    
    
-    const [revDatabyID, setRevbyIDData] = useState([])
+    const [comments, setComments] = useState([])
 
     const { review_id } = useParams();
 
     useEffect(() => {
-        getReviewsByID(review_id)
-        .then((reviews) => {setRevbyIDData(reviews)})
+        getCommentsByRID(review_id)
+        .then((comments) => {setComments(comments)})
     }, [])
 
-    console.log(revDatabyID)
+    console.log(comments)
+
+    
 
    
-
+ if (typeof(props.data) === 'string' && props.data !== '0') {
     
 
     return (
             <section className ="display">
+                <p> Comments</p>
              <ul className="ul">
                
-                {revDatabyID.map((data) => {
+                {comments.map((data) => {
                     return (
                         
-                        <li key={data.review_id}>
+                        <li key={data.comment_id}>
         
-                        <div className ="card2">
+                        <div className ="card3">
                        
-                            
+                    <p>"{data.body}"</p>
+                    <p className="right">{data.author}</p>
+                    <br></br>
+                    <p className="left">Votes: {data.votes}</p>
+                    <p className="left">{data.created_at}</p>
                     
-                    <img src = {data.review_img_url} alt={data.title} />
-                    <h2>{data.owner} reviewed:<strong> {data.title}</strong> by {data.designer}</h2>
-                    <br></br>
-                    <p>"{data.review_body}"</p>
-                    <br></br>
-                    <p>Votes: {data.votes}</p>
-                    <p >Comments: {data.comment_count} </p> 
-                    <p>{data.created_at}</p>
-                    <Link to={`/reviews`} className="link">
-                    <p>Click to return to all reviews</p>
-                    </Link>
     
                         </div>
     
@@ -62,4 +59,4 @@ export const Comments = (checkData) => {
              </ul>
             </section>
             )
-        }
+        }}
