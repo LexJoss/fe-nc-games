3e5055aecb171cac31c375600602c8b7d6f393a9
LexJoss <ajoss@hotmail.co.uk>
2023-02-10 12:54:41 +0000

    made all reviews page the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import {  Link } from 'react-router-dom';
 export const Home = () => {
     return (
 
-    <div>
+    <div className="display">
         <Link to={`/reviews`} className="text-link">
                     <p>Click to see to all reviews</p>
                     </Link>
